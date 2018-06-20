a377a06031966b0c269c20e79bac1bc0c03ec216
GLL <1300180236@qq.com>
2018-06-21 00:25:46 +0800

    函数参数类型、递归、lambda

--- basic_learning/function.py
+++ basic_learning/function.py
@@ -2,9 +2,11 @@
 # -*- coding:utf-8 -*-
 
 """
-有无参
-有无返回值
-位置参数，默认参数，可变参数（*args），关键字参数
+函数：
+    python内置函数
+    函数定义
+    函数参数：
+        位置参数(位置顺序)，默认参数(设置参数默认值，一般在最后)，可变参数（*args接收tuple），关键字参数(**kw接收dict)
 """
 
 
@@ -18,3 +20,48 @@ def b():
 
 
 b()
+
+
+# 默认参数
+# def mr(a, b=2):
+#     print(a + b)
+#
+#
+# mr(1, 3)
+
+# 可变参数
+# def kb(a, b, *args):
+#     c = a + b
+#     for i in args:
+#         c = c + i
+#     print(c)
+#
+#
+# t = (2, 3, 4, 1)
+# kb(1, 1, *t)
+
+# 关键字参数
+# def gjz(a, b, *args, **kwargs):
+#     c = 0
+#     for i in args:
+#         c = c + i
+#     for k, v in kwargs.items():
+#         d = k
+#         e = v
+#     print('name is:', a, 'age is:', str(b), 'score is:', str(c), d, 'is:', e)
+#
+#
+# t = (11, 22)
+# d = {'city': 'ty'}
+# gjz('air', 12, *t, **d)
+
+# 递归
+def dj(a):
+    if a == 1:
+        return a
+    else:
+        return a * dj(a - 1)
+
+
+print(dj(3))
+# lambda匿名函数
